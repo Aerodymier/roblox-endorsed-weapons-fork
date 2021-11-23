2929755e2bb74fad5377540e07725d9de76d8d98
Aerodymier <83131649+Aerodymier@users.noreply.github.com>
2021-11-23 20:57:51 +0300

    Fixed errors

--- .remodel/main.lua
+++ .remodel/main.lua
@@ -1,5 +1,6 @@
-local game = remodel.readPlaceFile("src/place.rbxl")
+local game = remodel.readPlaceFile("../StudioPlace/place.rbxl")
 
+local Workspace = game.Workspace
 local ReplicatedStorage = game.ReplicatedStorage
 local ServerScriptService = game.ServerScriptService
 local ServerStorage = game.ServerStorage
@@ -7,22 +8,26 @@ local StarterPack = game.StarterPack
 local StarterPlayer = game.StarterPlayer
 local StarterPlayerScripts = StarterPlayer.StarterPlayerScripts
 
+for _, model in ipairs(Workspace:GetChildren()) do
+    remodel.writeModelFile(model, "../src/workspace/" .. model.Name .. ".rbxmx")
+end
+
 for _, model in ipairs(ReplicatedStorage:GetChildren()) do
-    remodel.writeModelFile(model, "src/shared/" .. model.Name .. ".rbxmx")
+    remodel.writeModelFile(model, "../src/shared/" .. model.Name .. ".rbxmx")
 end
 
 for _, model in ipairs(ServerScriptService:GetChildren()) do
-    remodel.writeModelFile(model, "src/server/" .. model.Name .. ".rbxmx")
+    remodel.writeModelFile(model, "../src/server/" .. model.Name .. ".rbxmx")
 end
 
 for _, model in ipairs(ServerStorage:GetChildren()) do
-    remodel.writeModelFile(model, "src/storage/" .. model.Name .. ".rbxmx")
+    remodel.writeModelFile(model, "../src/storage/" .. model.Name .. ".rbxmx")
 end
 
 for _, model in ipairs(StarterPack:GetChildren()) do
-    remodel.writeModelFile(model, "src/starterpack/" .. model.Name .. ".rbxmx")
+    remodel.writeModelFile(model, "../src/starterpack/" .. model.Name .. ".rbxmx")
 end
 
 for _, model in ipairs(StarterPlayerScripts:GetChildren()) do
-    remodel.writeModelFile(model, "src/client/" .. model.Name .. ".rbxmx")
+    remodel.writeModelFile(model, "../src/client/" .. model.Name .. ".rbxmx")
 end
