f8cc68ebbc378b5010358de616182c3498e9d7d5
Aerodymier <83131649+Aerodymier@users.noreply.github.com>
2021-11-23 19:12:25 +0300

    Initial commit

---
+++ .remodel/main.lua
@@ -0,0 +1,28 @@
+local game = remodel.readPlaceFile("src/place.rbxl")
+
+local ReplicatedStorage = game.ReplicatedStorage
+local ServerScriptService = game.ServerScriptService
+local ServerStorage = game.ServerStorage
+local StarterPack = game.StarterPack
+local StarterPlayer = game.StarterPlayer
+local StarterPlayerScripts = StarterPlayer.StarterPlayerScripts
+
+for _, model in ipairs(ReplicatedStorage:GetChildren()) do
+    remodel.writeModelFile(model, "src/shared/" .. model.Name .. ".rbxmx")
+end
+
+for _, model in ipairs(ServerScriptService:GetChildren()) do
+    remodel.writeModelFile(model, "src/server/" .. model.Name .. ".rbxmx")
+end
+
+for _, model in ipairs(ServerStorage:GetChildren()) do
+    remodel.writeModelFile(model, "src/storage/" .. model.Name .. ".rbxmx")
+end
+
+for _, model in ipairs(StarterPack:GetChildren()) do
+    remodel.writeModelFile(model, "src/starterpack/" .. model.Name .. ".rbxmx")
+end
+
+for _, model in ipairs(StarterPlayerScripts:GetChildren()) do
+    remodel.writeModelFile(model, "src/client/" .. model.Name .. ".rbxmx")
+end
