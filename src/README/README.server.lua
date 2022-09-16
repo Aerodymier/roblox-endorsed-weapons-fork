450d554b23dd06a88ded46d56cfc6f14f44b6e99
Aerodymier <83131649+Aerodymier@users.noreply.github.com>
2022-09-17 00:56:49 +0300

    Initial commit and CI test

---
+++ src/README/README.server.lua
@@ -0,0 +1,7 @@
+--[[
+DELETE THIS AFTER YOU READ IT!
+
+Ungroup these models after moving them to the server their name tells. If a model's name is "ReplicatedStorage", drag that to ReplicatedStorage and ungroup the model.
+I really recommend you to watch the tutorial I uploaded to my YouTube channel.
+Definitely check the GitHub referances in that video for latest updates about the gun kit.
+]]
